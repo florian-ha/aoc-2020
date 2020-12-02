e68dcf60b570daddfea87422f1ea7fd1bc07a2f3
Florian-Ha <florian@familie-hasenauer.de>
2020-12-02 14:57:58 +0100

    added day02

---
+++ day02/day02.py
@@ -0,0 +1,40 @@
+with open('day02input.txt') as f:
+    inputData = f.read().splitlines()
+
+validPasswords = 0
+print("Exercise 1: ")
+for x in range(len(inputData)):
+    tempCache = inputData[x].split(" ")
+    tempNumbers = tempCache[0].split("-")
+    tempNumbers = list(map(int, tempNumbers))
+    tempLetter = tempCache[1].replace(":", "")
+    tempPassword = tempCache[2]
+    tempCounter = 0
+    for letter in tempPassword:
+        if letter == tempLetter:
+            tempCounter += 1
+    if (tempCounter >= tempNumbers[0]) and (tempCounter <= tempNumbers[1]):
+        validPasswords += 1
+print(validPasswords)
+
+validPasswords2 = 0
+print("Exercise 2: ")
+for x in range(len(inputData)):
+    validPasswordCount = 0
+    tempCache = inputData[x].split(" ")
+    tempNumbers = tempCache[0].split("-")
+    tempNumbers = list(map(int, tempNumbers))
+    tempLetter = tempCache[1].replace(":", "")
+    tempPassword = tempCache[2]
+    if tempLetter in tempPassword:
+        y = 0
+        for letter in tempPassword:
+            y += 1
+            if letter == tempLetter:
+                if y == tempNumbers[0] and y == tempNumbers[1]:
+                    x = 0
+                elif y == tempNumbers[0] or y == tempNumbers[1]:
+                    validPasswords2 += 1
+
+# sollte 360 ergeben
+print(validPasswords2)
