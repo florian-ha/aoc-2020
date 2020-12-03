0d8509b42eb443dc610c440715eaa0593c144cdb
Florian H <60797109+florian-ha@users.noreply.github.com>
2020-12-03 12:26:35 +0100

    Added day03

---
+++ day03/day03.py
@@ -0,0 +1,25 @@
+with open('day03input.txt') as f:
+    inputData = f.read().splitlines()
+
+
+def treeTraverse(right, down):
+    y = 0
+    tree_count = 0
+    for x in range(len(inputData)):
+        if (x % down) == 0:
+            temp_line = inputData[x]
+            if y >= 31:
+                y -= 31
+            if temp_line[y] == '#':
+                tree_count += 1
+            y += right
+
+    return tree_count
+
+
+print("Exercise 1: ")
+print(treeTraverse(3, 1))
+
+allSlopeMult = treeTraverse(3, 1) * treeTraverse(5, 1) * treeTraverse(7, 1) * treeTraverse(1, 2) * treeTraverse(1, 1)
+print("Exercise 2: ")
+print(allSlopeMult)
